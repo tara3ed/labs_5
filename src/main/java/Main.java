4af0b466f531379433554e1d527e1c33830a4150
Валентин Шляпужников <tara3ed@gmail.com>
2025-05-17 16:27:06 +0600

    Changed class FindPhone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+    import phones.FindPhone;
     import phones.Phone;
     import phones.PhoneEx;
 
--- src/main/java/phones/FindPhone.java
+++ src/main/java/phones/FindPhone.java
@@ -1,44 +1,66 @@
 package phones;
-import java.awt.Color;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FindPhone {
     private List<Phone> data=new PhoneDB().getPhoneData();
-    
+
+    public List<Phone> find(List<Tag> tags) {
+        List<Phone> result = new LinkedList<>();
+        for (Phone phone : data) {
+            boolean matchesAllTags = true;
+            for (Tag tag : tags) {
+                if (!tag.find(phone)) {
+                    matchesAllTags = false;
+                    break;
+                }
+            }
+            if (matchesAllTags) {
+                result.add(phone);
+            }
+        }
+        return result;
+    }
+
     public List<Phone> byColor(Color col) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getColor()==col)   find.add(ph);
-        return find;
+        List<Tag> tags =new ArrayList<>();
+        tags.add(new ColorTag(col));
+        return find(tags);
     }
     
     public List<Phone> byModel(String mod) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod))   find.add(ph);
-        return find;
+        List<Tag> tags=new ArrayList<>();
+        tags.add(new ModelTag(mod));
+        return find(tags);
     }
     
     public List<Phone> byMSize(int memSize) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getMemorySize()==memSize)   find.add(ph);
-        return find;
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new MSizeTag(memSize));
+        return find(tags);
     }
     
     public List<Phone> byModelAndPriceLow(String mod, double price) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod) &&
-                                ph.getPrice()<price)   find.add(ph);
-        return find;
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new ModelTag(mod));
+        tags.add(new PriceTag(price));
+        return find(tags);
     }
     
     public List<Phone> byMSizeAndNotColor(int memSize, Color col) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getMemorySize()==memSize && !(ph.getColor()==col))   find.add(ph);
-        return find;
-    }    
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new MSizeTag(memSize));
+        tags.add(new NotTag(new ColorTag(col)));
+        return find(tags);
+    }
+
+    public List<Phone> byModelAndPriceLowAndColor(String mod, double price, Color color) {
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new ModelTag(mod));
+        tags.add(new PriceTag(price));
+        tags.add(new ColorTag(color));
+        return find(tags);
+    }
 }
