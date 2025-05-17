ea635cd1eef2492f8c4c9c4697fcd8bb879c460c
Валентин Шляпужников <tara3ed@gmail.com>
2025-05-17 15:56:44 +0600

    Added class ModelTag

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
-import phones.Phone;
-import phones.PhoneEx;
+    import phones.Phone;
+    import phones.PhoneEx;
 
-import java.awt.*;
+    import java.awt.*;
 
-public class Main {
-    public static void main(String[] args) {
+    public class Main {
+        public static void main(String[] args) {
 
-        Phone ph1=new Phone("okii" ,4  ,1550, Color.RED,16);
-        System.out.println(ph1);
+            Phone ph1=new Phone("okii" ,4  ,1550, Color.RED,16);
+            System.out.println(ph1);
 
-        PhoneEx ph2=new PhoneEx();
-        System.out.println(ph2);
+            PhoneEx ph2=new PhoneEx();
+            System.out.println(ph2);
+        }
     }
-}
